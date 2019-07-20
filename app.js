ffcb194135d9af2526e66ef57de24f513b09dce8
Shahab13 <sam.autom@gmail.com>
2019-07-20 10:06:08 -0700

    basic server setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const { /* MONGO_DB,*/ NODE_ENV, PORT } = process.env;
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const app = express();
+
+/*if (MONGO_DB) {
+  const options = { useFindAndModify: false, useNewUrlParser: true };
+  mongoose.connect(MONGO_DB, options);
+  console.log("Connected to database!");
+} else {
+  console.log("MONGO_DB is not provided");
+}
+
+if (NODE_ENV === "development") app.use(morgan("dev"));
+app.use(require("body-parser").json());
+
+app.use("/api/courses", require("./api/routes/courses"));
+
+app.use(
+  ({ status = 500, message = "Something went wrong" }, req, res, next) => {
+    res.status(status).json({ status, message });
+  }
+);*/
+
+const listener = () => console.log("You are doing a thing!");
+app.listen(PORT, listener);
